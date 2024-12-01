219fb959f2b4786a3d770ecbcddbab7fe8cfb588
josevitor555 <koservitor123@gmail.com>
2024-12-01 18:46:18 -0300

    Projeto Backend com MongoDB

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -4,7 +4,7 @@ export default async function main() {
   try {
     mongoose.set('strictQuery', true);
     
-    await mongoose.connect("mongodb+srv://josevitordesousa123:I1Usr8VlFwLJn8mE@cluster0.hogjt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    await mongoose.connect("STRING_CONNETION_MONGODB");
 
     console.log('Connected to MongoDB!');
   } catch (error) {
